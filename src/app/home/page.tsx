8801f6795126b8bed9e20ce6008982bcf435123b
CodyMan0 <hys83751952@gmail.com>
2023-12-29 14:59:04 +0900

    feat : declare target type

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,14 +4,14 @@ import { Nav } from '@/components/sections/nav';
 
 import StarBg from './startBg';
 
-// export const test = {
-//   name: 'juyoung',
-// };
+export const test = {
+  name: 'juyoung',
+};
 
 const Home = () => {
   return (
     <div>
-      <Nav />
+      <Nav user={test} />
       <StarBg />
     </div>
   );
