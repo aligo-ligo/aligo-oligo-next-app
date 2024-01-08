4a99073e2b0288f7eab78ce5a3d301186c4a31f7
CodyMan0 <hys83751952@gmail.com>
2024-01-08 13:40:46 +0900

    design : specific target create page ui

--- src/features/target/components/create/SpecificForm.tsx
+++ src/features/target/components/create/SpecificForm.tsx
@@ -12,23 +12,19 @@ import { NEW_GOAL_FORM_ORDERS } from '../../constants';
 // import { DateInput } from './DateInput';
 import FormHeader from './FormHeader';
 import FormLayout from './FormLayout';
+import { TextInput } from './TextInput';
+import { useOverlay } from '@toss/use-overlay';
+import GoalGuideBottomSheet from './GoalGuideBottomSheet';
 
 export const SpecificForm = () => {
-  const { register, getValues, control } = useFormContext<TargetType>();
+  const overlay = useOverlay();
+  const { register, setValue, getValues, control } = useFormContext<TargetType>();
 
-  const {
-    fields: subGoal,
-    // append: subGoalAppend,
-    // remove: subGoalRemove,
-  } = useFieldArray({
-    name: 'subGoal', // unique name for your Field Array
-  });
   const { field } = useController({ name: 'goal', control });
   const { value } = field;
   console.log('etsetset', value);
   const { goal } = getValues();
 
-  console.log('subGoal', subGoal);
   return (
     <FormLayout
       header={<FormHeader formNumber={NEW_GOAL_FORM_ORDERS.specific} />}
@@ -40,8 +36,29 @@ export const SpecificForm = () => {
         </Typography>
       }
       body={
-        <div {...register('subGoal')} className="pt-sm">
-          {/* <DateInput maxLength={MAX_DATE_LENGTH_UNTIL_MONTH} onChange={onChange} /> */}
+        <div className="pt-sm h-full flex flex-col">
+          <SubGoalSection />
+          <RoutineSection />
+          <div className="pb-lg flex flex-col justify-end grow items-center gap-5xs">
+            <Typography type="caption1" className="text-gray-40">
+              목표 세우기, 너무 막연하다면?
+            </Typography>
+            <div className="w-[200px]">
+              <Button
+                className="py-3xs px-xs text-sm"
+                variant="green"
+                height="h44"
+                rounded="xl"
+                onClick={() => {
+                  overlay.open(({ isOpen, close }) => {
+                    return <GoalGuideBottomSheet open={isOpen} onClose={close} setValue={setValue} />;
+                  });
+                }}
+              >
+                가이드 보고 목표 세우기
+              </Button>
+            </div>
+          </div>
         </div>
       }
       footer={
@@ -51,5 +68,50 @@ export const SpecificForm = () => {
         </Link>
       }
     />
+  );
+};
+
+export const SubGoalSection = () => {
+  const { register, getValues, control } = useFormContext<TargetType>();
+  const {
+    // fields: subGoal,
+    // append: subGoalAppend,
+    // remove: subGoalRemove,
+  } = useFieldArray({
+    name: 'subGoal', // unique name for your Field Array
+  });
+  return (
+    <>
+      <TextInput
+        labelName="하위 목표"
+        value={'test'}
+        maxLength={100}
+        placeholder="ex) IT 개발 동아리 시작하기"
+        // onChange={onChange}
+      />
+    </>
+  );
+};
+
+export const RoutineSection = () => {
+  const { register, getValues, control } = useFormContext<TargetType>();
+  const {
+    // fields: routine,
+    // append: routineAppend,
+    // remove: routineRemove,
+  } = useFieldArray({
+    // shouldUnregister: true, 컴포넌트가 사라지면 unRegister이 되도록 !
+    name: 'routine', // unique name for your Field Array
+  });
+  return (
+    <div className="pt-sm">
+      <TextInput
+        labelName="루틴 작성"
+        value={'test'}
+        maxLength={100}
+        placeholder="ex) IT 개발 동아리 시작하기"
+        // onChange={onChange}
+      />
+    </div>
   );
 };
